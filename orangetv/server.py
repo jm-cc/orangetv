bf5ca996349dfe03b0ef9426de3a3c2c000d4683
COUTEYEN, Jean-marie <jean-marie.couteyen-carpaye@airbus.com>
2020-04-27 00:00:35 +0200

    sanitize a little bit the input when using the channel name

--- orangetv/server.py
+++ orangetv/server.py
@@ -9,6 +9,15 @@ app = flask.Flask("Orange")
 tv = orange.OrangeTV(IP_ORANGETV)
 tv.load_channel_mapping(MAPPING)
 
+purge = ["sur"] # Spurious words can be added by the vocal assistant.
+
+def sanitize(string):
+    res = string.lower()
+    for w in purge:
+        if res.startswith(w):
+            res=res[len(w):]
+    return res.strip()
+
 @app.route("/switchoff")
 def switch_off():
     tv.switch_off()
@@ -21,8 +30,8 @@ def switch_to_n(channel_id):
 
 @app.route("/channel_name", methods=["POST"])
 def switch_to_name():
-    data = flask.request.data.decode()
-    tv.switch_over_name(str(data))
+    data = sanitize(flask.request.data.decode())
+    tv.switch_over_name(data)
     return data
 
 if __name__ == "__main__":
